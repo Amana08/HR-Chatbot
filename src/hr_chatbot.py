39b8a6983edb55916b4932ed5a5d1da2cfeddbde
Amana08 <amanashariqkhan@gmail.com>
2025-03-24 10:39:33 +0530

    changed port of  public url

--- src/hr_chatbot.py
+++ src/hr_chatbot.py
@@ -268,4 +268,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.launch(share=True, server_name="0.0.0.0", server_port=8811)
+    app.launch(share=True, server_name="0.0.0.0", server_port=8890)
